acee15434fee55ca815ffb84c41c1a933a520c38
Nicolas Fricke <mail@nicolasfricke.de>
2014-06-04 00:17:25 +0200

    TMDb-u: Make messages durable, change route

--- tmdb_updater/lib/tmdb_updater/msg_publisher.rb
+++ tmdb_updater/lib/tmdb_updater/msg_publisher.rb
@@ -8,8 +8,8 @@ class MsgPublisher
   def enqueue_id(type, id)
     @connection.start
     channel = @connection.create_channel
-    queue   = channel.queue "raw_db.tmdb.#{type.to_s}"
-    channel.default_exchange.publish id.to_s, routing_key: queue.name
+    queue   = channel.queue "lom.raw_db.tmdb.#{type.to_s}", durable: true
+    queue.publish id.to_s, persistent: true
     @connection.close
   end
 end
