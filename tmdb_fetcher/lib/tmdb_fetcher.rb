ec65b7c5b4505126c31c2336749a8a9f3394c349
Nicolas Fricke <mail@nicolasfricke.de>
2014-06-04 00:08:22 +0200

    TMDb-f: Make messages durable, change route
    Durable messages: prevents RabbitMQ dropping messages if rabbitmq-server
    should be stopped, messages will be persisted on disk

--- tmdb_fetcher/lib/tmdb_fetcher.rb
+++ tmdb_fetcher/lib/tmdb_fetcher.rb
@@ -3,6 +3,3 @@ require_relative 'tmdb_fetcher/msg_publisher'
 
 movie_fetcher = Fetcher.new
 puts movie_fetcher.fetch_movie_ids verbose: true
-
-# msg_publisher = MsgPublisher.new
-# msg_publisher.enqueue_id :movie_id, 123
--- tmdb_fetcher/lib/tmdb_fetcher/msg_publisher.rb
+++ tmdb_fetcher/lib/tmdb_fetcher/msg_publisher.rb
@@ -8,8 +8,8 @@ class MsgPublisher
   def enqueue_id(type, id)
     @connection.start
     channel = @connection.create_channel
-    queue   = channel.queue "source.tmdb.#{type.to_s}"
-    channel.default_exchange.publish id.to_s, routing_key: queue.name
+    queue   = channel.queue "lom.source.tmdb.#{type.to_s}", durable: true
+    queue.publish id.to_s, persistent: true
     @connection.close
   end
 end
