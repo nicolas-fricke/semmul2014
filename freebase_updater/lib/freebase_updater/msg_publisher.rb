9f55f1938f1225d88891d96d047e3b20a85f00d5
Johannes Jasper <johannes.jasper@student.hpi.uni-potsdam.de>
2014-06-19 19:13:22 +0200

    Basic functionality Lots to do...

---
+++ freebase_updater/lib/freebase_updater/msg_publisher.rb
@@ -0,0 +1,23 @@
+require 'bunny'
+
+class FreebaseUpdater::MsgPublisher
+  def initialize
+    @connection = Bunny.new
+  end
+
+  def name
+    'freebase'
+  end
+
+  def queue_name(type)
+    "lom.source.#{name}.#{type.to_s}"
+  end
+
+  def enqueue_id(type, id)
+    @connection.start
+    channel = @connection.create_channel
+    queue   = channel.queue queue_name(type), durable: true
+    queue.publish id.to_s, persistent: true
+    @connection.close
+  end
+end
