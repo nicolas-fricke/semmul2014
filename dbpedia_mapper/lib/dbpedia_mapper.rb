354a702d5981f429eb89eee27aadb842a5130355
Johannes Wolf <janno42@gmail.com>
2014-07-05 21:36:46 +0200

    DBpediaMapper: cleanup

--- dbpedia_mapper/lib/dbpedia_mapper.rb
+++ dbpedia_mapper/lib/dbpedia_mapper.rb
@@ -1,6 +1,5 @@
 module DBpediaMapper
   require_relative 'dbpedia_mapper/mapper'
-  require_relative 'dbpedia_mapper/msg_consumer'
   require_relative '../../general/virtuoso_writer'
   require_relative '../../general/virtuoso_reader'
   require_relative '../../general/msg_consumer'
