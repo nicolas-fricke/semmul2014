c5c7c8b2bfcd8a128a119e81a32b3dcdb2b4f9ee
Nicolas Fricke <mail@nicolasfricke.de>
2014-07-11 19:51:19 +0200

    TMDb Fetcher: Minor: added more verbosity

--- tmdb_fetcher/bin/tmdb_fetcher_runner.rb
+++ tmdb_fetcher/bin/tmdb_fetcher_runner.rb
@@ -2,7 +2,12 @@ require_relative '../lib/tmdb_fetcher'
 
 if ARGV[0] == 'demo' and not ARGV[1].nil?
   puts 'Started fetcher in demo mode. Please type [yes] for fetching the demo movies.'
-  TMDbFetcher::Demo.new(ARGV[1]) if STDIN.gets(3) == 'yes'
+  if STDIN.gets(3) == 'yes'
+    TMDbFetcher::Demo.new(ARGV[1])
+    puts 'Enqueued demo movies!'
+  else
+    puts 'Aborted!'
+  end
 else
   movie_fetcher = TMDbFetcher::Fetcher.new
   puts movie_fetcher.fetch_movie_ids verbose: true
